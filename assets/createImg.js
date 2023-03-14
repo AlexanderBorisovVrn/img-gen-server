1631564cec078d6e8cbb06f2126fb2ba630a59c4
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-03-14 15:17:29 +0300

    feat:catch errors

--- assets/createImg.js
+++ assets/createImg.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config()
-import { OpenAIApi,Configuration } from "openai";
+import { OpenAIApi, Configuration } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.AI_KEY,
 });
@@ -8,16 +8,18 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
+export async function createImg(params) {
+  const { prompt, imgCount, size } = params;
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: imgCount,
+      size,
+      response_format: 'b64_json'
+    });
+    return response
 
-
-export async function createImg (params){
-  const {prompt,imgCount,size}=params;
-
-  const response = await openai.createImage({
-    prompt,
-    n:imgCount,
-    size,
-    response_format:'b64_json'
-  });
-  return response.data.data[0].b64_json;
+  } catch (error) {
+    console.log('error is in createImg ' + error)
+  }
 }
--- index.js
+++ index.js
@@ -16,29 +16,34 @@ const coreOpt = {
   origin: false,
   methods: ["POST"],
 }
-server.register(cors,{})
+server.register(cors, {})
 let port = parseInt(process.env.PORT, 10);
 
 // Declare a route
 server.get('/', async (request, reply) => {
- return "CREATED"
+  return "CREATED"
 })
 
 
 
 server.post('/creation', async (request, reply) => {
   const params = JSON.parse(request.body);
-  return await createImg(params);
- })
+  try {
+    const response = await createImg(params);
+    reply.send(response.data.data[0].b64_json);
+  } catch (error) {
+    console.log('error is in index.js ' + error);
+  }
+})
 
- 
+
 // Run the server!
 const start = async () => {
   try {
     await server.listen({
       port
     });
-    // console.log("server started on " + port + ' port')
+    console.log("server started on " + port + ' port')
   } catch (err) {
     process.exit(1)
   }
