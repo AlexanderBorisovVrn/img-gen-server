abc6dcac5bfbbb7d0b55617c6c13a33aa96ff7eb
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-03-20 15:08:09 +0300

    fix:mock image creator

--- assets/mockImageCreator.js
+++ assets/mockImageCreator.js
@@ -1,8 +1,13 @@
+import { Buffer } from 'buffer';
 import fetch from "node-fetch";
 const url = new URL('https://picsum.photos/512/512')
 
-export const mockImageCreator = async ()=>{
+export const mockImageCreator = async () => {
   const response = await fetch(url);
-  const buf = new Buffer.from(response.url,'utf-8')
-  return buf.toString('base64')
+  const img = await (await fetch(response.url)).arrayBuffer();
+  const buf = Buffer.from(img, 'utf-8').toString('base64')
+  return buf;
 }
+
+
+mockImageCreator();
